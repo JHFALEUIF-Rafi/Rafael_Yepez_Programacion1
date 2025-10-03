183b6b186f78112de121c1489f1a700696d95a4b
rafael yepez <xander21yl@gmail.com>
2025-10-03 08:37:35 -0500

    Termine el programa que calcula promedio

--- Promedio.cpp
+++ Promedio.cpp
@@ -1,44 +1,120 @@
 #include <iostream>
 using namespace std;
 
-void ingresarCalificacion()
+void ingresarCalificacion(float calificaciones[], int &cantidad)
 {
-    int asd;
+    float nota;
+    char continuar;
+
+    do
+    {
+        cout << "Ingrese la calificacion (1-10): ";
+
+        while (!(cin >> nota))
+        {
+            cout << "Ingrese una opcion valida (1-10)" << endl;
+            cout << "Ingrese la calificacion (1-10): ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+
+        if (nota < 1 || nota > 10)
+        {
+            cout << "\nPor favor, ingrese la calificacion (1-10)" << endl;
+            continue;
+        }
+
+        calificaciones[cantidad] = nota;
+        cantidad++;
+        cout << "Calificacion: " << nota << " agregada exitosamente." << endl;
+        cin.ignore(1000, '\n');
+
+        bool entradaValida = false;
+        do
+        {
+            cout << "Desea ingresar otra calificacion? (s/n): ";
+            cin >> continuar;
+            cin.ignore(1000, '\n');
+
+            if (continuar == 's' || continuar == 'S' || continuar == 'n' || continuar == 'N')
+            {
+                entradaValida = true;
+            }
+            else
+            {
+                cout << "Por favor ingrese 's' para si o 'n' para no." << endl;
+            }
+        } while (!entradaValida);
+
+    } while ((continuar == 's' || continuar == 'S') && cantidad < 100);
+
+    cout << "Total de calificaciones ingresadas: " << cantidad << endl;
 }
 
-void menu()
+void calcularPromedio(float calificaciones[], int cantidad)
+{
+    if (cantidad == 0)
+    {
+        cout << "No hay calificaciones ingresadas...." << endl;
+        return;
+    }
+
+    float suma = 0;
+    for (int i = 0; i < cantidad; i++)
+    {
+        suma += calificaciones[i];
+    }
+
+    double promedio = suma / cantidad;
+    int promedioEntero = promedio * 100;
+    promedio = promedioEntero / 100.0;
+    cout << "El promedio de las " << cantidad << " calificaciones es: " << promedio << endl;
+}
+
+void menu(float calificaciones[], int &cantidad)
 {
     int opcion;
     bool validar = true;
-    cout << "Seleccione una opcion:" << endl;
-    cout << "1. Ingresar Calificacion" << endl;
-    cout << "2. Obtener Promedio" << endl;
-    cout << "3. Mostrar Nota del Progreso" << endl;
-    cin >> opcion;
 
-    while (validar)
+    do
     {
+        cout << "\nSeleccione una opcion:" << endl;
+        cout << "1. Ingresar Calificacion" << endl;
+        cout << "2. Obtener Promedio" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Opcion: ";
+
+        while (!(cin >> opcion))
+        {
+            cout << "Ingrese una opcion valida (1-3)" << endl;
+            cout << "Opcion: ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+
         switch (opcion)
         {
         case 1:
-            cout << "1. Ingresar Alumno" << endl;
+            ingresarCalificacion(calificaciones, cantidad);
             break;
         case 2:
-            cout << "2. Calcular promedio" << endl;
+            calcularPromedio(calificaciones, cantidad);
             break;
         case 3:
-            cout << "Mostrar Alumno";
-            break;
-        default:
-            cout << "Ingrese opcion valida...";
+            cout << "Saliendo del programa..." << endl;
             validar = false;
             break;
+        default:
+            cout << "Opcion no valida, por favor intente de nuevo." << endl;
+            break;
         }
-    }
+    } while (validar);
 }
 
 int main()
 {
-    menu();
+    float calificaciones[100];
+    int cantidad = 0;
+    menu(calificaciones, cantidad);
     return 0;
 }
