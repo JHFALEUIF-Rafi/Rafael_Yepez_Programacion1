bc1a2244110b5ccffda54c3503426b78588e9666
rafael yepez <xander21yl@gmail.com>
2025-10-31 13:26:46 -0500

    como carajo me saque 8,5????

--- Ejercicios a parte/Electrodomestico.cpp
+++ Ejercicios a parte/Electrodomestico.cpp
@@ -5,9 +5,12 @@ int main()
 {
     const int aparatos = 3;
     const int dias = 7;
+    int mayor;
+    float suma;
     string electrodomesticos[aparatos] = {"Refrigerador", "Lavadora", "Televisor"};
     string diaSemana[dias] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
     float registro[aparatos][dias];
+    float consumoSemana[aparatos];
 
     for (int i = 0; i < aparatos; i++)
     {
@@ -41,6 +44,45 @@ int main()
             cout << registro[i][j] << "\t\t\t";
         }
         cout << endl;
+    }
+
+    // calcular consumo total
+    for (int i = 0; i < aparatos; i++)
+    {
+        for (int j = 0; j < dias; j++)
+        {
+            suma += registro[i][j];
+        }
+        consumoSemana[i] = suma;
+    }
+
+    // suma total consumo
+    int sumaTotal = 0;
+    for (int i = 0; i < aparatos; i++)
+    {
+        sumaTotal += consumoSemana[i];
+    }
+    cout << "\nEl total del consumo general por los tres electrodomesticos es: " << sumaTotal << endl;
+
+    //promedio
+    for (int i = 0; i < aparatos; i++)
+    {
+        cout << "El promedio del electrodomestico " << electrodomesticos[i] << " es " << consumoSemana[i] / dias << endl;
+    }
+
+    mayor = consumoSemana[0];
+    for (int i = 0; i < aparatos; i++)
+    {
+        if (consumoSemana[i] > mayor)
+        {
+            mayor = consumoSemana[i];
+        }
+        cout << "El electrodomestico que más consume es " << electrodomesticos[i] << " con un valor semanal de " << mayor << endl;
+    }
+
+    if (sumaTotal > 150)
+    {
+        cout << "Disminuye la energía" << endl;
     }
 
     return 0;
